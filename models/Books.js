6ee22d7ce9414664e2cc975069e2da49603b4302
AbdelrahmanHashem468 <abdelrahmanhashem1298@gmail.com>
2023-04-03 20:50:56 +0200

    AvgRate Schema

--- models/Books.js
+++ models/Books.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2');
 
-const booksSchema = new mongoose.Schema({
+const booksSchema = new mongoose.Schema(
+{
     name:{
         type: String,
         required: true
@@ -17,10 +18,33 @@ const booksSchema = new mongoose.Schema({
     authorId:{
         type: mongoose.Types.ObjectId,
         ref: 'Author'
+    },
+    totalRating:{
+        type: Number
+    },
+    ratingNumber:{
+        type: Number
     }
-},{
-    timestamps: true
-})
+},
+{
+    timestamps: true,        
+}
+)
+
+const virtual = booksSchema.virtual('avgRate');
+virtual.get(function(value, virtual, doc) {
+    return Number((this.totalRating/this.ratingNumber).toFixed(1));
+});
+
+booksSchema.set('toJSON', {
+    virtuals: true, 
+    transform: function (doc, ret) {
+        delete ret.totalRating;
+        delete ret.ratingNumber;
+        delete ret.id;
+    },
+});
+
 
 booksSchema.plugin(mongoosePaginate);
 
