c87d0929c0184d3784e8dcc218ff8c82b10f5777
AbdelrahmanHashem468 <abdelrahmanhashem1298@gmail.com>
2023-04-13 05:46:54 +0200

    prevent delete category or author with associated books

--- models/Authors.js
+++ models/Authors.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2');
-
+const Book = require('./Books');
+const { BaseError } = require('../libs');
 const authorSchema = new mongoose.Schema({
     firstName: {
         type: String,
@@ -25,6 +26,19 @@ const authorSchema = new mongoose.Schema({
 })
 
 authorSchema.plugin(mongoosePaginate);
+
+
+authorSchema.pre('findOneAndDelete', async function preDelete(next) {
+    const authorId = this.getQuery()['_id'];
+    const count =  await Book.countDocuments({authorId:authorId});
+    if (count > 0) {
+        const err = new BaseError('Cannot delete author with associated books',409);
+        next(err);
+    } else {
+        next();
+    }
+})
+
 
 const Author = mongoose.model('Author', authorSchema);
 
--- models/Categories.js
+++ models/Categories.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2');
-
+const Book = require('./Books');
+const { BaseError } = require('../libs');
 const CategoriesSchema = new mongoose.Schema({
 
     Name: {
@@ -18,6 +19,16 @@ const CategoriesSchema = new mongoose.Schema({
 
 CategoriesSchema.plugin(mongoosePaginate);
 
+CategoriesSchema.pre('findOneAndDelete', async function preDelete(next) {
+    const categoryId = this.getQuery()['_id'];
+    const count =  await Book.countDocuments({categoryId:categoryId});
+    if (count > 0) {
+        const err = new BaseError('Cannot delete category with associated books',409);
+        next(err);
+    } else {
+        next();
+    }
+})
 
 const Category = mongoose.model('Category', CategoriesSchema);
 
